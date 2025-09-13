7c5b33349eef607e6efcdbae45b1e883ba1370ac
meghabhansali2911 <meghabhansali2529@gmail.com>
2025-09-13 22:41:44 +0530

    Update

--- src/utility/firebase.js
+++ src/utility/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./job-vibes-3c48b-firebase-adminsdk-fbsvc-3ead25c9fa.json");
+const serviceAccount = require("./privatekey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
