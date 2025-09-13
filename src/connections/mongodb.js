932fd5964563c01febba61de90ceee3205b01e28
meghabhansali2911 <meghabhansali2529@gmail.com>
2025-09-13 16:30:13 +0530

    updated mongo

--- src/connections/mongodb.js
+++ src/connections/mongodb.js
@@ -1,27 +1,27 @@
 const mongoose = require("mongoose");
 
 const URL = process.env.MONGO_URI || "mongodb://localhost:27017/jobvibes";
-
-mongoose.set('strictQuery', false);
+console.log("URL--", URL);
+mongoose.set("strictQuery", false);
 mongoose
-    .connect(URL, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    })
-    .then(() => {
-        console.log("---mongodb connection successfully---");
-    })
-    .catch((error) => {
-        console.log("---DB Connection ERROR--", error);
-    });
+  .connect(URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("---mongodb connection successfully---");
+  })
+  .catch((error) => {
+    console.log("---DB Connection ERROR--", error);
+  });
 
 function cleanup() {
-    mongoose.connection.close(function () {
-        console.log('Mongoose connection closed.');
-        process.exit(0);
-    });
+  mongoose.connection.close(function () {
+    console.log("Mongoose connection closed.");
+    process.exit(0);
+  });
 }
 
-process.on('SIGINT', cleanup);
-process.on('exit', cleanup);
-process.on('SIGTERM', cleanup);
+process.on("SIGINT", cleanup);
+process.on("exit", cleanup);
+process.on("SIGTERM", cleanup);
