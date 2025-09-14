29be8dea0b3dbf4dc944181f4c770344bc4ccdaf
meghabhansali2911 <meghabhansali2529@gmail.com>
2025-09-14 19:14:25 +0530

    Remove file

--- src/utility/firebase.js
+++ src/utility/firebase.js
@@ -1,8 +1,8 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./privatekey.json");
+// const admin = require("firebase-admin");
+// const serviceAccount = require("./privatekey.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
-module.exports = admin;
+// module.exports = admin;
